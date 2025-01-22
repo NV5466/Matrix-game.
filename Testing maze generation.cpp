d88bc0c37086be03d632f64612c66d93a75a6064
NV5466 <wolfaden49@gmail.com>
2025-01-21 19:12:59 -0500

    Update Testing maze generation.cpp
    Bruh

--- Testing maze generation.cpp
+++ Testing maze generation.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <random>
+std::vector<std::vector<int>> extrapMap = {
+            {1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,1},
+            {1,0,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1}
+        };
+
 
 
 int GRInt(int min, int max){
@@ -12,52 +23,61 @@ int GRInt(int min, int max){
 }
 
 //Testing pass by reference for a randomization for the map.
-void blockRanRefTest(int &Er, int &Ec){
-    std::vector<std::vector<int>> block = {
+class blockRand2{
+    public:
+
+
+
+    void blockRandiC2(std::vector<std::vector<int>> x,int size){
+        std::vector<std::vector<int>> block = {
             {0,0,0},
             {0,0,0},
             {0,0,0}
-    };
-
-    bool isCol = GRInt(0,1);
-    int columnNum;
-    int rowNum;
-    if (isCol == 1){
-        columnNum = GRInt(0,2);
-        for (int i=0; i<=2; i++){
-                block[i][columnNum]=1;
+        };
+        bool isCol = GRInt(0,1);
+        int columnNum;
+        int rowNum;
+        if (isCol == 1){
+            columnNum = GRInt(0,2);
+            for (int i=0; i<=2; i++){
+                    block[i][columnNum]=1;
+                }
+        }
+        
+        else{
+            rowNum=GRInt(0,2);
+            for (int i=0; i<=2; i++){
+                    block[rowNum][i]=1;
             }
+        }
+        //Displaying the block. Just for testing.
+        // for(int i =0; i<=2; i++){
+        //    for(int j = 0; j<=2; j++){
+        //        std::cout << block[i][j] << " ";
+        //    }
+        //    std::cout << std::endl;
+        //}
+        //Debug for block func
+        int rt;
     }
     
-    else{
-        rowNum=GRInt(0,2);
-        for (int i=0; i<=2; i++){
-                block[rowNum][i]=1;
-        }
+    void change(std::vector<std::vector<int>>& x,std::vector<<std::vector<int>>& block, int Er, int Ec){
+        x[Er-1][Ec-1] = block[0][0];
+        x[Er-1][Ec] = block[0][1];
+        x[Er-1][Ec+1] = block[0][2];
+
+        x[Er][Ec-1] = block[1][0];
+        x[Er][Ec] = block[1][1];
+        x[Er][Ec+1] = block[1][2];
+
+        x[Er+1][Ec-1] = block[2][0];
+        x[Er+1][Ec] = block[2][1];
+        x[Er+1][Ec+1] = block[2][2];
     }
-    //Displaying the block. Just for testing.
-    // for(int i =0; i<=2; i++){
-    //    for(int j = 0; j<=2; j++){
-    //        std::cout << block[i][j] << " ";
-    //    }
-    //    std::cout << std::endl;
-    //}
-    //Debug for block func
+};
 
 
 
-}
-
-std::vector<std::vector<int>> extrapMap = {
-            {1,1,1,1,1,1,1,1},
-            {1,0,0,0,0,0,0,1},
-            {1,0,0,0,0,0,0,1},
-            {1,0,0,0,0,0,0,1},
-            {1,0,0,0,0,0,0,1},
-            {1,0,0,0,0,0,0,1},
-            {1,0,0,0,0,0,0,1},
-            {1,1,1,1,1,1,1,1}
-        };
 
 //Code that is all bunched together with ducttape and sticks.
 void blockRan2(){
@@ -144,13 +164,22 @@ int main(){
     //displays extrap map in int main
     //testing for size
     for (int i=0; i<extrapMap.size(); i++){
-        std::cout<< i;
         for (int j=0; j<extrapMap[i].size(); j++){
             std::cout<<extrapMap[i][j]<< " ";
         }
         std::cout << std::endl;
     }
-    //displays extrap map
+    //displays extrap map^^^^
+    //
+
+    std::cout<<extrapMap.size();
+
+    //my value to test while creating the pbr func for block rand.
+    int actualSize = extrapMap.size() - 2;
+
+
+    blockRand2 h;
+    h.blockRandiC2();
 
 
 
